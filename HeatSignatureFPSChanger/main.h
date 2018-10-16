16f693eca8cd0a0578be58642848e123ae1920bd
Chris Yeninas <phantom@sundergaming.com>
2018-10-16 17:27:33 -0400

    Initial commit

---
+++ HeatSignatureFPSChanger/main.h
@@ -0,0 +1,48 @@
+#pragma once
+#include <iostream>
+#include <Windows.h>
+#include <tlhelp32.h>
+#include <Psapi.h>
+
+void WriteToMemory(uintptr_t addressToWrite, char* valueToWrite, int byteNum)
+{
+	unsigned long OldProtection;
+	VirtualProtect((LPVOID)(addressToWrite), byteNum, PAGE_EXECUTE_READWRITE, &OldProtection);
+	memcpy((LPVOID)addressToWrite, valueToWrite, byteNum);
+	VirtualProtect((LPVOID)(addressToWrite), byteNum, OldProtection, NULL);
+}
+
+MODULEINFO GetModuleInfo(char *szModule)
+{
+	MODULEINFO modinfo = { 0 };
+	HMODULE hModule = GetModuleHandle(szModule);
+	if (hModule == 0)
+		return modinfo;
+	GetModuleInformation(GetCurrentProcess(), hModule, &modinfo, sizeof(MODULEINFO));
+	return modinfo;
+}
+
+DWORD FindPattern(char *module, char *pattern, char *mask)
+{
+	MODULEINFO mInfo = GetModuleInfo(module);
+	DWORD base = (DWORD)mInfo.lpBaseOfDll;
+	DWORD size = (DWORD)mInfo.SizeOfImage;
+
+	DWORD patternLength = (DWORD)strlen(mask);
+
+	for (DWORD i = 0; i < size - patternLength; i++)
+	{
+		bool found = true;
+		for (DWORD j = 0; j < patternLength; j++)
+		{
+			found &= mask[j] == '?' || pattern[j] == *(char*)(base + i + j);
+		}
+
+		if (found)
+		{
+			return base + i;
+		}
+	}
+
+	return NULL;
+}
